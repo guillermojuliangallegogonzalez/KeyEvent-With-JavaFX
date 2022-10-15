3675d762eea365e6898a909ad78f300542db2fa3
Guillermo Gallego Gonzalez <114193449+guillermojuliangallegogonzalez@users.noreply.github.com>
2022-10-15 13:38:46 +0200

    KeyEvent with JavaFX

---
+++ src/keyeventmovement/KeyEventMovement.java
@@ -0,0 +1,42 @@
+package keyeventmovement;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class KeyEventMovement extends Application {
+  @Override 
+  public void start(Stage primaryStage) {
+    
+    Pane pane = new Pane();
+    Text text = new Text(150, 150, "A");
+    
+    pane.getChildren().add(text);
+    text.setOnKeyPressed(e -> {          
+      switch (e.getCode()) {
+        case DOWN: text.setY(text.getY() + 10); break;
+        case UP:  text.setY(text.getY() - 10); break;
+        case LEFT: text.setX(text.getX() - 10); break;
+        case RIGHT: text.setX(text.getX() + 10); break;
+        default: 
+          if (e.getText().length() > 0)
+            text.setText(e.getText());
+      }
+    });
+    
+    
+    Scene scene = new Scene(pane);
+    primaryStage.setTitle("KeyEventMovement");
+    primaryStage.setScene(scene); 
+    primaryStage.show(); 
+    
+    text.requestFocus(); 
+  }
+
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+} 
